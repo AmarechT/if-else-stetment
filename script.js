ee1bd1446cf83281ab7409b3cc172392131af7b5
Amarecht <Amarechtato22@gmail.com>
2022-03-25 10:31:56 -0500

    ifelse

---
+++ script.js
@@ -0,0 +1,25 @@
+function age(age){
+    let name
+    if(age==3){
+        name ="kena"
+    }
+   else if(age==6){
+       name="nathan"
+
+    }
+    else if(age==8){
+        name="abraham"
+    }
+    else if(age==30){
+         name ="amarech"
+    }
+    else if(age==31){
+          name="Dessu"
+    }
+   else {
+       name = "nokids"
+   }
+    return name
+}
+
+console.log(age(31))
